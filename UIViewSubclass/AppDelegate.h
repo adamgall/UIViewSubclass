907e221a1967dbace7ec23e2589f2a5cf9e6390d
Adam Gall <galladamj@gmail.com>
2016-03-24 14:52:38 -0400

    Initial Commit

---
+++ UIViewSubclass/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIViewSubclass
+//
+//  Created by Adam Gall on 3/24/16.
+//  Copyright © 2016 AcmeCo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIViewSubclass/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIViewSubclass
+//
+//  Created by Adam Gall on 3/24/16.
+//  Copyright © 2016 AcmeCo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
